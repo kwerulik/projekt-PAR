a1008af35112112340dfacaa85061468b32a1bed
WhiteRed14 <konarzewski100@gmail.com>
2025-06-12 14:11:34 +0200

    api cors fix

--- api/CarRepairApi/Program.cs
+++ api/CarRepairApi/Program.cs
@@ -19,9 +19,10 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500")
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:*", "http://localhost:*")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
